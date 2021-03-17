c408217147d98b58006b0c223f3c13db1634656d
Alexander Skvortsov <ascvorcov@gmail.com>
2021-03-18 02:11:22 +0300

    Fixed index error, removed gradual license price restrictions

--- GoldDigger/App.cs
+++ GoldDigger/App.cs
@@ -141,9 +141,9 @@ namespace GoldDigger
 
 					var cost = _coins.Count switch
 					{
-						>= 10000 => 21,
-						>= 5000 => 11,
-						>= 1000 => 5,
+						//>= 10000 => 21,
+						//>= 5000 => 11,
+						>= 1000 => 21,
 						_ => 1
 					};
 
@@ -634,7 +634,7 @@ namespace GoldDigger
 							{
 								license.Discard();
 								foreach (var tr in treasures)
-									_recoveredTreasures[map.Depth].Enqueue(new TreasureChest {Id = tr, FromLevel = map.Depth});
+									_recoveredTreasures[map.Depth-1].Enqueue(new TreasureChest {Id = tr, FromLevel = map.Depth});
 
 								Interlocked.Add(ref _treasureDugOutTotal, treasures.Length);
 								map.Depth++;
